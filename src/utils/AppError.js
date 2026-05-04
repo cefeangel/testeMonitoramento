08e9b64bd41b3644c1db5a77c906cb13a538d14d
Angelo Gonçalves <cefeangel@hotmail.com>
2026-05-04 10:51:09 -0300

    feat: adicionar verificação de links quebrados

--- src/utils/AppError.js
+++
@@ -1,7 +0,0 @@
-export class AppError extends Error {
-  constructor(message, statusCode = 400, errors = []) {
-    super(message);
-    this.statusCode = statusCode;
-    this.errors = errors;
-  }
-}
