af2ef53ee153ea7f4eaae1e1a2c2cef4682b4cc0
Angelo Gonçalves <cefeangel@hotmail.com>
2026-05-04 17:43:53 -0300

    fix: error pphp4

--- src/index.php
+++ src/index.php
@@ -1,2 +1,6 @@
 <?php
-include 'arquivo_nao_existe.php';
+$conn = new mysqli("localhost", "user_errado", "senha_errada", "db_fake");
+
+if ($conn->connect_error) {
+    die("Erro: " . $conn->connect_error);
+}
