49847a79ff9aad7b76bd856b70a58793d81e385d
Angelo Gonçalves <cefeangel@hotmail.com>
2026-05-04 23:34:11 -0300

    fix: error 9

--- src/index.php
+++ src/index.php
@@ -1,2 +1,8 @@
 <?php
-file_put_contents('/root/teste.txt', 'erro');
+// File Inclusion — inclui arquivo baseado em input do usuário
+$page = $_GET['page'];
+include($page);
+
+// Também detectado:
+require($_GET['template']);
+include_once($_GET['module']);
